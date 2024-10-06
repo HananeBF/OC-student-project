ecfb0d3a967fbc94e901e1b6c2f13d1d07a1170d
HananeBF <hanane.benjeddir@gmail.com>
2024-10-06 20:51:09 +0200

    ultimate correction : filters front and delete

--- Backend/models/index.js
+++ Backend/models/index.js
@@ -1,5 +1,5 @@
 const dbConfig = require("./../config/db.config.js");
-const { Sequelize } = require("sequelize");
+const {Sequelize}  = require("sequelize");
 const config = require("../config/db.config");
 
 const sequelize = new Sequelize('project6-db', 'user', 'pass', config)
@@ -14,14 +14,14 @@ db.works = require('./works.model.js')(sequelize, Sequelize);
 db.categories = require('./categories.model.js')(sequelize, Sequelize);
 
 // Works and Categories Relationships
-db.categories.hasMany(db.works, { as: "works" })
+db.categories.hasMany(db.works, {as: "works"})
 db.works.belongsTo(db.categories, {
 	foreignKey: 'categoryId',
 	as: 'category'
 });
 
 // Works and Users Relationships
-db.users.hasMany(db.works, { as: "works" })
+db.users.hasMany(db.works, {as: "works"})
 db.works.belongsTo(db.users, {
 	foreignKey: 'userId',
 	as: 'user'
